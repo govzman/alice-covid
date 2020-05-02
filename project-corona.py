01e97a990267178bf7cb957584f5e3db6b71f264
govzman <58818686+govzman@users.noreply.github.com>
2020-05-02 21:58:07 +0300

    Add files via upload

---
+++ project-corona.py
@@ -0,0 +1,104 @@
+# -"- coding:utf-8 -"-
+
+import requests
+import json
+import os
+
+from flask import Flask, request
+import logging
+
+
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+sessionStorage = {}
+
+part_dialog = 0  # 0 - узнаем адрес, 1 - говорим количество
+#                  и ближайший адрес, 2 - спрашиваем хочет ли узнать еще
+
+'''
+geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Красная площадь,+1&format=json"
+
+response = requests.get(geocoder_request)
+if response:
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    print(toponym_coodrinates)
+else:
+    print("Ошибка выполнения запроса:")
+    print(geocoder_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+'''
+
+
+@app.route('/', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(request.json, response)
+    logging.info(f'Response:  {response!r}')
+    return json.dumps(response)
+
+
+
+
+'''
+req = requests.post('https://coronavirus.mash.ru/search.php',
+                    data={'referal': 'Улица'})
+streets = req.text.split('</tr>')[1:]
+streets.pop()
+for i in streets:
+    print(''.join(i.split('</td>')).split('<td>')[1:3])
+'''
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+    if req['session']['new']:
+        part = 0
+        res['response']['text'] = 'Привет! Узнай сколько заболевших коронавирусом есть около тебя! Назови адрес, про который хочешь узнать (работает только для Москвы)'
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Красная площадь, 1",
+                "Закончить диалог",
+            ]
+        }
+        res['response']['buttons'] = sessionStorage[user_id]
+        return
+
+    if req['request']['original_utterance'].lower() == 'закончить диалог':
+        res['response']['text'] = 'Пока! Возвращайся за новой информацией завтра!'
+        res['response']['end_session'] = True
+        return
+    
+    if req['request']['entities'][0]['value']['street'] == '' and part == 0:
+        res['response'][
+            'text'] = 'Я не совсем поняла вас. Назовите адрес в Москве'
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Красная площадь, 1",
+                "Закончить диалог",
+            ]
+        }
+        res['response']['buttons'] = sessionStorage[user_id]
+    elif part == 0 and req['request']['entities'][0]['value']['street'] != '':
+        part = 1
+        res['response']['text'] = req['request']['entities'][0]['value']
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
