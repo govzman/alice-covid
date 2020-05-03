27af54e3896fe9d227febc2562f7cc79d56ed4f0
govzman <58818686+govzman@users.noreply.github.com>
2020-05-03 22:11:50 +0300

    Update project-corona.py

--- project-corona.py
+++ project-corona.py
@@ -138,7 +138,7 @@ def handle_dialog(req, res):
                 "hide": True
             }]
         except Exception as e:
-            res['response']['text'] = 'Ошибка: " + str(e) + '  '+ str(e.__class__.__name__) + ". Назовите адрес еще раз'
+            res['response']['text'] = "Ошибка: " + str(e) + "  " + str(e.__class__.__name__) + ". Назовите адрес еще раз"
             res['response']['buttons'] = [{
                 "title": "Красная площадь, 1",
                 "payload": {},
