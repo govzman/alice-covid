37182abb2ac3100031f125bc875dc0c8a3352452
govzman <58818686+govzman@users.noreply.github.com>
2020-05-06 22:55:46 +0300

    Update project-corona.py

--- project-corona.py
+++ project-corona.py
@@ -61,7 +61,7 @@ def search(cords, rad=1):
         # случай, когда заболевших не найдено
         text = 'В радиусе километра зараженных не обнаружено, поздравляю!'
         # спрашивает хочет ли еще узнать
-    text += ' Хочешь узнать еще о каком-нибудь месте?'
+    text += ' Назови новый адрес или скажи "Завершить диалог"'
     return text
 
 app = Flask(__name__)
@@ -90,70 +90,64 @@ def main():
     return json.dumps(response)
 
   
-def handle_dialog(req, res):
+def handle_dialog(req, res): # диалог с пользователем
     user_id = req['session']['user_id']
-    if req['session']['new']:
-        parts[user_id] = 0
+    if req['session']['new']: # новая сессия
         res['response'][
-            'text'] = 'Привет! Узнай сколько заболевших коронавирусом есть около тебя! Назови адрес, про который хочешь узнать, но помни, что я могу сказать только про Москву и окрестности'
+            'text'] = 'Привет! Узнай сколько заболевших коронавирусом есть около тебя! ' + \
+            'Назови адрес, про который хочешь узнать, правда я могу сказать только про Москву и окрестности'
         return
 
-    try:
-        if list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['закончить', 'диалог'] or list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['завершить', 'диалог']:
+    try: # на всякий случай)
+        if list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['закончить', 'диалог'] or \
+           list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['завершить', 'диалог']:
             res['response']['text'] = 'Пока! Возвращайся за новой информацией завтра и старайся поменьше выходить из дома!'
             res['response']['tts'] = 'Пока! Возвращайся за новой информацией завтра и старайся поменьше выход+ить из дома!'
-            parts[user_id] = 0
             res['response']['end_session'] = True
             return
     except Exception:
         pass
     try:
-        if list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['что', 'ты', 'умеешь'] or list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['помощь']:
-            res['response']['text'] = 'Я умею определять сколько зараженных коронавирусом людей находятся в радиусе 1 километра от тебя, для этого мне достаточно сказать адрес любого дома. Скажи мне адрес'
-            parts[user_id] = 0
+        # что ты умеешь? - обязательный вопрос в навыке Алисы
+        if list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['что', 'ты', 'умеешь'] or \
+           list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['помощь']:
+            res['response']['text'] = 'Я умею определять сколько зараженных коронавирусом людей находятся в ' + \
+                'радиусе 1 километра от тебя, для этого мне достаточно сказать адрес любого дома. Скажи мне адрес'
             return
     except Exception:
         pass
-    if parts[user_id] == 0:
-        try:
-            for i, dat in enumerate(req['request']['nlu']['entities']):
-                if dat['type'] == "YANDEX.GEO":      
-                    if 'city' not in list(req['request']['nlu']['entities'][i]['value'].keys()):
-                        geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Москва,+" + \
-                            req['request']['nlu']['entities'][i]['value']['street'] + \
-                            ",+" + req['request']['nlu']['entities'][i]['value']['house_number'] + "&format=json"
-                    else:
-                        geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Москва,+" + \
-                            req['request']['nlu']['entities'][i]['value']['city'] + ',+' + \
-                            req['request']['nlu']['entities'][i]['value']['street'] + \
-                            ",+" + req['request']['nlu']['entities'][i]['value']['house_number'] + "&format=json"
-                    response = requests.get(geocoder_request)
-                    if response:
-                        json_response = response.json()
-                        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                        toponym_coordinates = toponym["Point"]["pos"]
-                        res['response']['text'] = search(
-                            toponym_coordinates.split())
-                        parts[user_id] = 1                       
-                    else:
-                        res['response']['text'] = 'Мне очень жаль, но такого адреса нет, скажите еще раз'
-                    return
-            res['response']['text'] = 'Кажется, это не адрес. Назовите адрес еще раз'
-        except Exception as e:
-            res['response']['text'] = "Не совсем тебя поняла. Назови адрес еще раз"
-    else:
-        if 'нет' in list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) or 'не хочу' in list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])):
-            res['response']['text'] = 'Пока! Возвращайся за новой информацией завтра и старайся поменьше выходить из дома!'
-            res['response']['tts'] = 'Пока! Возвращайся за новой информацией завтра и старайся поменьше выход+ить из дома!'
-            parts[user_id] = 0
-            res['response']['end_session'] = True      
-            return
-        elif 'да' in list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) or 'давай' in list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])):
-            parts[user_id] = 0
-            res['response']['text'] = 'Назови адрес'
-        else:
-            res['response']['text'] = 'Ты не определился, скажи да или нет' 
-            res['response']['tts'] = 'Ты не определился, скажи да sil <[300]> или нет'
+    try:
+        for i, dat in enumerate(req['request']['nlu']['entities']):
+            if dat['type'] == "YANDEX.GEO":
+                if 'city' not in list(req['request']['nlu']['entities'][i]['value'].keys()):
+                    # ищем спрашиваемый дом через API Яндекс.Карт
+                    geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Москва,+" + \
+                        req['request']['nlu']['entities'][i]['value']['street'] + \
+                        ",+" + \
+                        req['request']['nlu']['entities'][i]['value']['house_number'] + \
+                        "&format=json"
+                else:
+                    # случай, если будут спрашивать соседние города (Химки, Мытищи и тд)
+                    geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Москва,+" + \
+                        req['request']['nlu']['entities'][i]['value']['city'] + ',+' + \
+                        req['request']['nlu']['entities'][i]['value']['street'] + \
+                        ",+" + \
+                        req['request']['nlu']['entities'][i]['value']['house_number'] + \
+                        "&format=json"
+                response = requests.get(geocoder_request)
+                if response:  # если адрес нашелся
+                    json_response = response.json()
+                    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                    toponym_coordinates = toponym["Point"]["pos"]
+                    res['response']['text'] = search(
+                        toponym_coordinates.split())
+                else:  # если не нашелся
+                    res['response']['text'] = 'Мне очень жаль, но такого адреса нет, скажите еще раз'
+                return
+        # пользователь сказал не адрес
+        res['response']['text'] = 'Кажется, это не адрес. Назовите адрес еще раз'
+    except Exception as e:
+        res['response']['text'] = "Не совсем тебя поняла. Назови адрес еще раз"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
