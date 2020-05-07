cb212c66fbeef8a5722b4bc4f252897c84698cbd
govzman <58818686+govzman@users.noreply.github.com>
2020-05-07 19:05:08 +0300

    Update project-corona.py

--- project-corona.py
+++ project-corona.py
@@ -8,18 +8,20 @@ from flask import Flask, request
 import logging
 import sqlite3
 
+con = sqlite3.connect('data_covid.db')  # база данных
+cur = con.cursor()
 
 def search(cords, rad=1):
+    global con, cur
     count = 0  # количество где расстояние меньше километра
     dis = []  # список из дистанций
-    con = sqlite3.connect('data_covid.db')  # база данных
-    cur = con.cursor()
+    
     cords[0] = int(cords[0].replace('.', '').ljust(10, '0'))
     cords[1] = int(cords[1].replace('.', '').ljust(10, '0'))
     result = cur.execute(
         "SELECT width, height, adress FROM adresses WHERE (width >= " + str(cords[0] - 9000000 * rad) + ") AND (width <= " + str(cords[0] + 9000000 * rad) +
         ") AND (height >= " + str(cords[1] - 1562500 * rad) + ") AND (height <= " + str(cords[1] + 1562500 * rad) + ")").fetchall()
-    con.close()
+    
     for i in result:  # считаем расстояния через разницу координат
         distance = ((
             (i[1] - cords[1]) / 900) ** 2 + (
